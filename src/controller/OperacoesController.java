4142316a6477c9988ee48155afdee93092b45a05
vnasa <vnasa@LAPTOP-VETU9ACB>
2021-08-21 13:18:00 -0300

    primeiro_exercicio

---
+++ src/controller/OperacoesController.java
@@ -0,0 +1,25 @@
+package controller;
+
+import sun.util.resources.ext.CurrencyNames_no_NO;
+
+public class OperacoesController {
+
+	public OperacoesController() {
+		super();
+	}
+
+	
+// Concatenar 32768 caracteres 	
+    public void concatenaString() {
+    
+    	String sequencia = "";
+    	double tempoInicial =  System.nanoTime();
+    	
+    	
+    }
+    
+	
+}
+
+//<ctrl + S> ele salva a class que esta aberta some o * o * significa que a class nao esta salva
+// <ctrl + Shift + s> -salva todas abas abertas class
---
+++ src/view/Principal.java
@@ -0,0 +1,14 @@
+package view;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+//main + <ctrl + espaço> - cria o main
+// ctrl + espaco 
+
